3a4f220b1e8ea145da1ccc7ec1be4113e30b85d1
Adeel Abbas <adeel444abbas@gmail.com>
2024-04-25 12:26:53 +0500

    Opimzed the code

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const button = document.querySelector(".fetch-data");
+const pagination = document.querySelector(".pagination");
 const previousBtn = document.querySelector(".prev-page");
 const nextBtn = document.querySelector(".next-page");
 const pageInfo = document.querySelector(".page-info");
+
+nextBtn.classList.add("disp-none");
+pageInfo.classList.add("disp-none")
+previousBtn.classList.add("disp-none")
+
 
 let currentPage = 1;
 const postsPerPage = 5;
@@ -18,7 +24,7 @@ const handlePosts = async () => {
   addLoader();
   document.querySelector(".stories").innerHTML = "";
   try {
-    let response = await axios.get("https://.algolia.com/api/v1/search");  // https://hn.algolia.com/api/v1/search
+    let response = await axios.get("https://hn.algolia.com/api/v1/search"); // https://hn.algolia.com/api/v1/search
     console.log(response);
     if (response.status == 200) {
       removeLoader();
@@ -49,9 +55,18 @@ const handlePosts = async () => {
 
 const updatePage = (totalPosts) => {
   const totalPages = Math.ceil(totalPosts / postsPerPage);
+  pageInfo.classList.remove("disp-none");
+  nextBtn.classList.remove("disp-none");
   pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
-  previousBtn.disabled = currentPage === 1;
-  nextBtn.disabled = currentPage === totalPages;
+  
+  if(currentPage  > 1){
+    previousBtn.classList.remove("disp-none");
+  } else{
+    previousBtn.classList.add("disp-none");
+  }
+  if(currentPage === 4){
+    nextBtn.classList.add("disp-none")
+  }
 };
 
 button.addEventListener("click", handlePosts);
